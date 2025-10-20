cf2b163c57451010552c67f14787f6d0083c1184
Keks <keks@htmlacademy.ru>
2025-10-20 12:44:50 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/data.js
@@ -0,0 +1,21 @@
+import {getRandomArrayElement, getRandomInteger} from './util.js';
+import {NAMES, MESSAGES, DESCRIPTIONS} from './constants.js';
+
+const createComment = (id) => ({
+  id: id,
+  avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
+  message: Array.from({length: getRandomInteger(1, 2)}, () => getRandomArrayElement(MESSAGES)).join(' '),
+  name: getRandomArrayElement(NAMES)
+});
+
+const createPost = (id) => ({
+  id: id,
+  url: `photos/${id}.jpg`,
+  description: getRandomArrayElement(DESCRIPTIONS),
+  likes: getRandomInteger(15, 200),
+  comments: Array.from({length: getRandomInteger(0, 30)}, (_, i) => createComment(i + id * 1000))
+});
+
+const PHOTOS = Array.from({length: 25}, (_, i) => createPost(i + 1));
+
+export {PHOTOS};
---
+++ 5/js/main.js
@@ -0,0 +1,3 @@
+import {PHOTOS} from './data.js';
+
+console.log(PHOTOS);
